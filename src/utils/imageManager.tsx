dd457a177b2e2b2b1a7fefae385b553939a262d7
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-03-25 12:02:40 +0200

    MASFE2024S-75 Added import/export for images

---
+++ src/utils/imageManager.tsx
@@ -0,0 +1,103 @@
+import logoHorizontal from './../../public/images/logo/logo-horizontal-large.svg';
+import logoVertical from './../../public/images/logo/logo-vertical-small.svg';
+
+import loginIllustration1 from './../../public/images/illustrations/login-illustration-1.png';
+import loginIllustration2 from './../../public/images/illustrations/login-illustration-2.png';
+import loginIllustration3 from './../../public/images/illustrations/login-illustration-3.png';
+
+import alarmClockImage from './../../public/images/graphics/alarm-clock.png';
+import basketImage from './../../public/images/graphics/basket.png';
+import okHandImage from './../../public/images/graphics/ok-hand.png';
+import questionMarkImage from './../../public/images/graphics/question-mark.png';
+import thumbsUpImage from './../../public/images/graphics/thumbs-up.png';
+import throphy1Image from './../../public/images/graphics/trophy-1.png';
+import throphy2Image from './../../public/images/graphics/trophy-2.png';
+import throphy3Image from './../../public/images/graphics/trophy-3.png';
+
+import burgerImage from './../../public/images/graphics/food/burger.png';
+import chickenImage from './../../public/images/graphics/food/chicken.png';
+import cornImage from './../../public/images/graphics/food/corn.png';
+import friesImage from './../../public/images/graphics/food/fries.png';
+import hamburgerFriesImage from './../../public/images/graphics/food/hamburger-fries.png';
+import kebabImage from './../../public/images/graphics/food/kebab.png';
+import pizzaImage from './../../public/images/graphics/food/pizza.png';
+import pretzelImage from './../../public/images/graphics/food/pretzel.png';
+import ramenImage from './../../public/images/graphics/food/ramen.png';
+import sandwichImage from './../../public/images/graphics/food/sandwich.png';
+import soupImage from './../../public/images/graphics/food/soup.png';
+
+import addIcon from './../../public/images/icons/add.svg';
+import arrowFilledIcon from './../../public/images/icons/arrow-filled.svg';
+import arrowFilledCircleIcon from './../../public/images/icons/arrow-circle-filled.svg';
+import arrowLongIcon from './../../public/images/icons/arrow-long.svg';
+import checkOutlinedIcon from './../../public/images/icons/check-outlined.svg';
+import checkFilledCircleIcon from './../../public/images/icons/check-circle-filled.svg';
+import chevronIcon from './../../public/images/icons/chevron.svg';
+import chilliIcon from './../../public/images/icons/chilli.svg';
+import closeIcon from './../../public/images/icons/close.svg';
+import deleteBinIcon from './../../public/images/icons/delete-bin.svg';
+import errorOutlinedIcon from './../../public/images/icons/error-outlined.svg';
+import gradingIcon from './../../public/images/icons/grading.svg';
+import infoOutlinedIcon from './../../public/images/icons/info-outlined.svg';
+import linkIcon from './../../public/images/icons/link.svg';
+import menuIcon from './../../public/images/icons/menu.svg';
+import plantIcon from './../../public/images/icons/plant.svg';
+import restartIcon from './../../public/images/icons/restart.svg';
+import restaurantIcon from './../../public/images/icons/restaurant.svg';
+import searchIcon from './../../public/images/icons/search.svg';
+import shoppingBasketIcon from './../../public/images/icons/shopping-basket.svg';
+import startFilledCircleIcon from './../../public/images/icons/star-circle-filled.svg';
+import starFullIcon from './../../public/images/icons/star-full.svg';
+import starHalfFullIcon from './../../public/images/icons/star-half-full.svg';
+import starEmptyIcon from './../../public/images/icons/start-empty.svg';
+
+export {
+    logoHorizontal,
+    logoVertical,
+    loginIllustration1,
+    loginIllustration2,
+    loginIllustration3,
+    burgerImage,
+    chickenImage,
+    cornImage,
+    friesImage,
+    hamburgerFriesImage,
+    kebabImage,
+    pizzaImage,
+    pretzelImage,
+    ramenImage,
+    sandwichImage,
+    soupImage,
+    alarmClockImage,
+    basketImage,
+    okHandImage,
+    questionMarkImage,
+    thumbsUpImage,
+    throphy1Image,
+    throphy2Image,
+    throphy3Image,
+    addIcon,
+    arrowFilledIcon,
+    arrowFilledCircleIcon,
+    arrowLongIcon,
+    checkOutlinedIcon,
+    checkFilledCircleIcon,
+    chevronIcon,
+    chilliIcon,
+    closeIcon,
+    deleteBinIcon,
+    errorOutlinedIcon,
+    gradingIcon,
+    infoOutlinedIcon,
+    linkIcon,
+    menuIcon,
+    plantIcon,
+    restartIcon,
+    restaurantIcon,
+    searchIcon,
+    shoppingBasketIcon,
+    startFilledCircleIcon,
+    starFullIcon,
+    starHalfFullIcon,
+    starEmptyIcon
+}

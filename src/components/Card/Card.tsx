ebde77617224fb2033d6b2f77830b2e1d2cde78d
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-03-28 17:38:38 +0200

    MASFE2024S-79 Customizable style properties

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,28 @@
 import { ReactElement, ReactNode } from 'react'
 import styles from "./Card.module.css";
 
+type Spacing = "xs" | "s" | "m"
 
-const Card = (): ReactElement => {
+type BoxShadow = "xs" | "s" | "m"
+
+type CardProps = {
+    children: ReactNode;
+    spacing?: Spacing;
+    shadow?: BoxShadow;
+};
+
+const getSpacing = (spacing?: Spacing): string => {
+    return spacing ? styles[`card__spacing_${spacing}`] : ""
+}
+
+const getBoxShadow = (boxShadow?: BoxShadow): string => {
+    return boxShadow ? styles[`card__boxShadow_${boxShadow}`] : ""
+}
+
+export const Card = ({ children, spacing, shadow }: CardProps): ReactElement => {
     return (
-        <div className={`${styles.card}`}>
+        <div className={`${styles.card} ${getSpacing(spacing)} ${getBoxShadow(shadow)}`}>
+            {children}
         </div>
     )
 }
-export default Card

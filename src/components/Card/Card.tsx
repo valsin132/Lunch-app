b1164d39715a1f077a855fc4d6c054743f43bd09
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-03-28 17:11:32 +0200

    MASFE2024S-79 Card component, default styling

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, ReactNode } from 'react'
+import styles from "./Card.module.css";
+
+
+const Card = (): ReactElement => {
+    return (
+        <div className={`${styles.card}`}>
+        </div>
+    )
+}
+export default Card

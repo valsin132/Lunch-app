e541d1d09354cc84acafd69a94b31b8095d4f3da
Ernest Rimkevicius <65626331+Urnesto@users.noreply.github.com>
2024-03-29 17:16:17 +0200

    MASFE2024S-78 Button text color, button size , disabled effect, width

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,35 +1,58 @@
 import React, { Children, ReactElement } from "react";
 import styles from './Button.module.css';
 
-type ButtonSize = "md" | "sm" | "xs"
+type ButtonSize = 'md' | 'sm' | 'xs'
 
-type ButtonColor = "white" | "blue" | "orange"
+type ButtonColor = 'white' | 'blue' | 'orange'
 
-type IconPosition = "left" | "right";
+type IconPosition = 'left' | 'right';
 
+type ButtonTextColor = 'white' | 'purple' | 'orange' | 'blue'
+
+
+type ButtonWidth = 'auto' | 'full';
 interface ButtonProps {
     title: string;
-    buttonSize?: ButtonSize;
+    buttonSize: ButtonSize;
     buttonColor: ButtonColor;
     icon?: string;
-    iconLocation?: 'left' | 'right'
+    iconPosition: IconPosition;
+    buttonTextColor: ButtonTextColor;
+    buttonWidth: ButtonWidth;
+    onClick?: () => void;
+    disabled?: boolean;
 }
 
 const getButtonSize = (buttonSize?: ButtonSize): string => {
-    return buttonSize ? styles[`button__size_${buttonSize}`] : ""
+    return buttonSize ? styles[`button__size-${buttonSize}`] : ''
 }
 
 const getButtonColor = (buttonColor?: ButtonColor): string => {
-    return buttonColor ? styles[`button__color_${buttonColor}`] : ""
+    return buttonColor ? styles[`button__color-${buttonColor}`] : ''
 }
 
-export const Button = ({ buttonSize, buttonColor, title, icon, iconLocation }: ButtonProps): ReactElement => {
+const getButtonTextColor = (buttonTextColor?: ButtonTextColor): string => {
+    return buttonTextColor ? styles[`button__text-${buttonTextColor}`] : ''
+}
 
-    const displayIconLeft = icon && iconLocation === 'left'
-    const displayIconRight = icon && iconLocation === 'right'
+const getButtonWidth = (buttonWidth?: ButtonWidth): string => {
+    return buttonWidth ? styles[`button__width-${buttonWidth}`] : ''
+}
+
+export const Button = ({ buttonSize, buttonColor, title, icon, iconPosition, buttonTextColor, buttonWidth, disabled, onClick }: ButtonProps): ReactElement => {
+
+    const displayIconLeft = icon && iconPosition === 'left'
+    const displayIconRight = icon && iconPosition === 'right'
 
     return (
-        <button className={`${styles.button} ${getButtonSize(buttonSize)} ${getButtonColor(buttonColor)}`} >
+        <button
+            className={`
+        ${styles.button} 
+        ${getButtonSize(buttonSize)} 
+        ${getButtonWidth(buttonWidth)}
+        ${getButtonTextColor(buttonTextColor)}
+        ${getButtonColor(buttonColor)}
+          `} onClick={onClick} disabled={disabled}>
             {displayIconLeft && (
                 <img src={icon} className={`${styles.button__icon} ${styles.button__icon__left}`} />
             )}

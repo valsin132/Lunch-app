ab2c2b35ecce56837946e9b3d63c4f28a75ac0b6
robert_dulko <robertdlk2@gmail.com>
2024-03-29 13:26:52 +0200

    MASFE2024S-89 Recolorable icons

--- src/utils/imageManager.tsx
+++ src/utils/imageManager.tsx
@@ -1,5 +1,3 @@
-import logoHorizontal from './../assets/images/logo/logo-horizontal-large.svg';
-import logoVertical from './../assets/images/logo/logo-vertical-small.svg';
 
 import loginIllustration1 from './../assets/images/illustrations/login-illustration-1.png';
 import loginIllustration2 from './../assets/images/illustrations/login-illustration-2.png';
@@ -26,34 +24,7 @@ import ramenImage from './../assets/images/graphics/food/ramen.png';
 import sandwichImage from './../assets/images/graphics/food/sandwich.png';
 import soupImage from './../assets/images/graphics/food/soup.png';
 
-import addIcon from './../assets/images/icons/add.svg';
-import arrowFilledIcon from './../assets/images/icons/arrow-filled.svg';
-import arrowFilledCircleIcon from './../assets/images/icons/arrow-circle-filled.svg';
-import arrowLongIcon from './../assets/images/icons/arrow-long.svg';
-import checkOutlinedIcon from './../assets/images/icons/check-outlined.svg';
-import checkFilledCircleIcon from './../assets/images/icons/check-circle-filled.svg';
-import chevronIcon from './../assets/images/icons/chevron.svg';
-import chilliIcon from './../assets/images/icons/chilli.svg';
-import closeIcon from './../assets/images/icons/close.svg';
-import deleteBinIcon from './../assets/images/icons/delete-bin.svg';
-import errorOutlinedIcon from './../assets/images/icons/error-outlined.svg';
-import gradingIcon from './../assets/images/icons/grading.svg';
-import infoOutlinedIcon from './../assets/images/icons/info-outlined.svg';
-import linkIcon from './../assets/images/icons/link.svg';
-import menuIcon from './../assets/images/icons/menu.svg';
-import plantIcon from './../assets/images/icons/plant.svg';
-import restartIcon from './../assets/images/icons/restart.svg';
-import restaurantIcon from './../assets/images/icons/restaurant.svg';
-import searchIcon from './../assets/images/icons/search.svg';
-import shoppingBasketIcon from './../assets/images/icons/shopping-basket.svg';
-import startFilledCircleIcon from './../assets/images/icons/star-circle-filled.svg';
-import starFullIcon from './../assets/images/icons/star-full.svg';
-import starHalfFullIcon from './../assets/images/icons/star-half-full.svg';
-import starEmptyIcon from './../assets/images/icons/start-empty.svg';
-
 export {
-    logoHorizontal,
-    logoVertical,
     loginIllustration1,
     loginIllustration2,
     loginIllustration3,
@@ -76,28 +47,4 @@ export {
     throphy1Image,
     throphy2Image,
     throphy3Image,
-    addIcon,
-    arrowFilledIcon,
-    arrowFilledCircleIcon,
-    arrowLongIcon,
-    checkOutlinedIcon,
-    checkFilledCircleIcon,
-    chevronIcon,
-    chilliIcon,
-    closeIcon,
-    deleteBinIcon,
-    errorOutlinedIcon,
-    gradingIcon,
-    infoOutlinedIcon,
-    linkIcon,
-    menuIcon,
-    plantIcon,
-    restartIcon,
-    restaurantIcon,
-    searchIcon,
-    shoppingBasketIcon,
-    startFilledCircleIcon,
-    starFullIcon,
-    starHalfFullIcon,
-    starEmptyIcon
 }

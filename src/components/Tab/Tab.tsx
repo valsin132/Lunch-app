5d4f3df63ef9425479e8153f868462ee2441ec18
RokasBe <rokas.bedalis@gmail.com>
2024-04-06 11:31:18 +0300

    comment fix, classNames

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,22 +1,30 @@
 import styles from "./Tab.module.css";
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
+import classNames from "classnames/bind";
 
+const cx = classNames.bind(styles);
 interface TabProps {
   label: string;
-  href: string;
+  onClick?: (e: MouseEvent) => void;
 }
-
-export const Tab: React.FC<TabProps> = ({ label, href }) => {
+export const Tab: React.FC<TabProps> = ({ label, onClick }) => {
   const [isActive, setIsActive] = useState(false);
 
-  const handleClick = () => {
+  const handleClick = (e: MouseEvent) => {
     setIsActive(!isActive);
+    if (onClick) {
+      onClick(e);
+    }
   };
+
   return (
-    <div className={isActive ? styles.containerActive : styles.container}>
-      <a href={href} className={styles.tab} onClick={handleClick}>
+    <div className={cx(`tab--container`)}>
+      <button
+        className={cx([`tab`], { ["tab--active"]: isActive })}
+        onClick={handleClick}
+      >
         {label}
-      </a>
+      </button>
     </div>
   );
 };

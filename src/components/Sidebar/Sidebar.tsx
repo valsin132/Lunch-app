96482ef78e1e2839129fd537071575c580137fb0
robert_dulko <robertdlk2@gmail.com>
2024-04-09 16:08:25 +0300

    MASFE2024S-92 Sidebar logo anims

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,27 @@
 import classNames from 'classnames/bind';
 import { useState } from 'react';
-import { ChevronIcon, LogoHorizontal, LogoVertical } from '../../utils/iconManager';
+import { ChevronIcon } from '../../utils/iconManager';
 import styles from './Sidebar.module.css';
 
 const cx = classNames.bind(styles);
 
 export function Sidebar() {
   const [isExpanded, setIsExpanded] = useState(false);
+
   const handleExpand = () => {
     setIsExpanded(!isExpanded);
   };
+
   return (
     <aside id="sidebar-nav" className={cx('sidebar')}>
-      <div className={cx('sidebar__container', { 'sidebar--collapsed': !isExpanded })}>
-        {isExpanded ? (
-          <LogoHorizontal className={cx('sidebar__logo')} />
-        ) : (
-          <LogoVertical className={cx('sidebar__logo')} />
-        )}
+      <div
+        className={cx('sidebar__container', {
+          'sidebar--collapsed': !isExpanded,
+          'sidebar--expanded': isExpanded,
+        })}>
+        <div className={cx('sidebar__logo-wrapper')}>
+          <div className={cx('sidebar__logo')} />
+        </div>
       </div>
       <button
         className={cx('sidebar__expand-btn')}

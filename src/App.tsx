14c32ce3f78211b84e05594829c40e7c1c4a3823
robert_dulko <robertdlk2@gmail.com>
2024-04-09 11:52:46 +0300

    MASFE2024S-92 styling, expand functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from './components/Sidebar/Sidebar';
 import styles from './App.module.css';
 
 export function App() {
@@ -5,6 +6,7 @@ export function App() {
     <div className={styles.container}>
       <h1>Welcome to Sourcery for FrontEnd 2024</h1>
       <p>You can delete all this HTML code and start working on your Lunch App</p>
+      <Sidebar />
     </div>
   );
 }

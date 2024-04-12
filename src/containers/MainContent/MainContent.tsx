638990c553e84ffc2f0916dffe809eafdd78f567
robert_dulko <robertdlk2@gmail.com>
2024-04-12 18:46:05 +0300

    MASFE2024S-90 Removed console.logs

--- src/containers/MainContent/MainContent.tsx
+++ src/containers/MainContent/MainContent.tsx
@@ -6,14 +6,10 @@ export function MainContent() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
 
   useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('mount');
     const timeout = setTimeout(() => {
       setIsLoggedIn(false);
     }, 10000);
     return () => {
-      // eslint-disable-next-line no-console
-      console.log('dismount');
       clearTimeout(timeout);
     };
   });

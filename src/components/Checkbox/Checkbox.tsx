48655279c9ec65a272b5bfdfbfaeaf5d3bf4f761
irminaj <irmina.janulyte@gmail.com>
2024-04-16 12:25:12 +0300

    Change checkbox classes naming

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -20,20 +20,20 @@ export function Checkbox({
   onChange,
 }: CheckboxProps): ReactElement {
   return (
-    <div className={cx('checkbox-container')}>
+    <div className={cx('checkbox')}>
       <input
         type="checkbox"
         id={id}
-        className={cx('checkbox-container__input', {
-          'checkbox-container__input--error': isError,
+        className={cx('checkbox__input', {
+          'checkbox__input--error': isError,
         })}
         disabled={isDisabled}
         onChange={onChange}
       />
       <label
-        className={cx('checkbox-container__label', {
-          'checkbox-container__label--error': isError,
-          'checkbox-container__label--disabled': isDisabled,
+        className={cx('checkbox__label', {
+          'checkbox__label--error': isError,
+          'checkbox__label--disabled': isDisabled,
         })}
         htmlFor={id}>
         {label}

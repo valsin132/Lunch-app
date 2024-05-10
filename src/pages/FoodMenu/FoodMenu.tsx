51b8e3fb004f5ae8563edf6633e6f2c9d5557be9
irminaj <irmina.janulyte@gmail.com>
2024-05-11 01:25:51 +0300

    Add dish details modal to food menu page

--- src/pages/FoodMenu/FoodMenu.tsx
+++ src/pages/FoodMenu/FoodMenu.tsx
@@ -3,12 +3,28 @@ import { ReactElement, useState, useMemo } from 'react';
 import { useFetchData } from '../../hooks/useFetchData';
 import { FoodCard } from '../../components/FoodCard';
 import { Tab } from '../../components/Tab';
-import { Meal, Vendor, Rating, WeekDay } from './FoodMenu.types';
+import { Meal, Vendor, Rating, WeekDay, Users } from './FoodMenu.types';
+import { DishDetailsModal, DishInfoProps } from '../../components/DishDetails';
 import styles from './FoodMenu.module.css';
 
 const cx = classNames.bind(styles);
 
+// eslint-disable-next-line max-lines-per-function
 export function FoodMenu(): ReactElement {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const [dishDetails, setDishDetails] = useState<DishInfoProps>({
+    vendor: '',
+    title: '',
+    isVegetarian: false,
+    isSpicy: false,
+    rating: '',
+    description: '',
+    price: 0,
+    dishType: 'bowl',
+    comments: [{ id: 0, comment: '', name: '', surname: '', userIcon: '' }],
+  });
+
   const {
     data: vendorsData,
     isLoading: vendorsLoading,
@@ -24,6 +40,11 @@ export function FoodMenu(): ReactElement {
     isLoading: ratingsLoading,
     isError: ratingsError,
   } = useFetchData<Rating[]>('http://localhost:3002/ratings');
+  const {
+    data: usersData,
+    isLoading: usersLoading,
+    isError: usersError,
+  } = useFetchData<Users[]>('http://localhost:3002/users');
 
   const [selectedDay, setSelectedDay] = useState<WeekDay>('Monday');
 
@@ -48,11 +69,39 @@ export function FoodMenu(): ReactElement {
     return 'Not rated';
   };
 
-  if (vendorsLoading || mealsLoading || ratingsLoading) {
+  const getUser = (id: number) => {
+    const user = usersData?.find((users) => Number(users.id) === id);
+    return user;
+  };
+
+  const getComments = (id: number) => {
+    const filteredComments = ratingsData?.filter((rating) => rating.mealId === id) ?? [];
+    if (filteredComments.length > 0) {
+      const comments = filteredComments.map((rating) => ({
+        comment: rating.rating.comment,
+        user: rating.rating.userId,
+      }));
+      const getCommentDetails = comments.map((comment, commentId) => {
+        const userDetails = getUser(comment.user);
+        const fullComment = {
+          id: commentId + 1,
+          comment: comment.comment,
+          name: userDetails?.name,
+          surname: userDetails?.surname,
+          userIcon: userDetails?.img,
+        };
+        return fullComment;
+      });
+      return getCommentDetails;
+    }
+    return undefined;
+  };
+
+  if (vendorsLoading || mealsLoading || ratingsLoading || usersLoading) {
     return <p>Loading...</p>;
   }
 
-  if (vendorsError || mealsError || ratingsError) {
+  if (vendorsError || mealsError || ratingsError || usersError) {
     return <p>Error fetching data!</p>;
   }
 
@@ -80,9 +129,41 @@ export function FoodMenu(): ReactElement {
             spicy={meal.spicy}
             rating={getRating(Number(meal.id))}
             dishType={meal.dishType}
-            onClick={onclick}
+            onClick={() => {
+              setDishDetails({
+                vendor: getVendorName(meal.vendorId),
+                title: meal.title,
+                description: meal.description,
+                price: meal.price,
+                isVegetarian: meal.vegetarian,
+                isSpicy: meal.spicy,
+                rating: getRating(Number(meal.id)),
+                dishType: meal.dishType,
+                comments: getComments(Number(meal.id)),
+              });
+              setIsOpenModal(true);
+            }}
           />
         ))}
+      </div>
+      <div>
+        {isOpenModal && (
+          <DishDetailsModal
+            vendor={dishDetails.vendor}
+            title={dishDetails.title}
+            description={dishDetails.description}
+            price={dishDetails.price}
+            isVegetarian={dishDetails.isVegetarian}
+            isSpicy={dishDetails.isSpicy}
+            rating={dishDetails.rating}
+            dishType={dishDetails.dishType}
+            comments={dishDetails.comments}
+            setIsOpen={() => {
+              setIsOpenModal(false);
+            }}
+            onClick={onclick}
+          />
+        )}
       </div>
     </div>
   );

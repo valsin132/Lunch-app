9588e98094aa2531e3ba8ddb5b3c513a8ba748d3
irminaj <irmina.janulyte@gmail.com>
2024-05-19 21:35:58 +0300

    Modify useLogin hook to log in immediately after registration without refreshing the page

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,37 +1,28 @@
 import { useState } from 'react';
-import { useFetchData } from './useFetchData';
-
-interface User {
-  email: string;
-  password: string;
-  name: string;
-  surname: string;
-  balance: number;
-  img: string;
-  orders: {
-    weekDay: string;
-    mealIds: number[];
-  }[];
-}
 
 export const useLogin = () => {
-  const { data: userData, isError: isFetchError } = useFetchData<User>(
-    'http://localhost:3002/user'
-  );
   const [errorMsg, setErrorMsg] = useState<string>('');
 
-  const login = (email: string, password: string, setShowToast: (value: boolean) => void): void => {
+  const login = async (
+    email: string,
+    password: string,
+    setShowToast: (value: boolean) => void
+  ): Promise<void> => {
     setErrorMsg('');
 
-    if (userData?.email === email && userData?.password === password) {
-      window.localStorage.setItem('userData', JSON.stringify(userData));
-      window.dispatchEvent(new Event('storage'));
-    } else {
-      setErrorMsg(
-        isFetchError
-          ? 'Login failed. Please try again later.'
-          : 'Incorrect email or password. Please try again.'
-      );
+    try {
+      const response = await fetch('http://localhost:3002/user');
+      const userData = await response.json();
+
+      if (userData?.email === email && userData?.password === password) {
+        window.localStorage.setItem('userData', JSON.stringify(userData));
+        window.dispatchEvent(new Event('storage'));
+      } else {
+        setErrorMsg('Incorrect email or password. Please try again.');
+        setShowToast(true);
+      }
+    } catch (_error) {
+      setErrorMsg('Login failed. Please try again later.');
       setShowToast(true);
     }
   };

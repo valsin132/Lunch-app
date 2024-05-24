e6f1c85bfefc0e00ff3ed94281b225338f7b2b52
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-05-24 16:20:50 +0300

    done NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import { Button } from '../../components/Button';
+import { Footer } from '../../components/Footer';
+import styles from './NotFoundPage.module.css';
+
+const cx = classNames.bind(styles);
+
 export function NotFoundPage() {
-  return <div>Not Found</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className={cx('not-found-page__wrapper')}>
+      <div className={cx('not-found-page__content')}>
+        <p className={cx('not-found-page__header')}>Oops!</p>
+        <p>Page not found!</p>
+        <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
+        <Button title="Go back" buttonType="primary" buttonSize="md" onClick={handleClick} />
+      </div>
+      <Footer />
+    </div>
+  );
 }

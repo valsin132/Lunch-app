2ca1935aa927f1b6f1a33cabba48b6c9ce46f8c5
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-05-24 17:49:27 +0300

    deleted separate function, add one-liner

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -9,17 +9,18 @@ const cx = classNames.bind(styles);
 export function NotFoundPage() {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate('/');
-  };
-
   return (
     <div className={cx('not-found-page__wrapper')}>
       <div className={cx('not-found-page__content')}>
         <p className={cx('not-found-page__header')}>Oops!</p>
         <p>Page not found!</p>
         <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
-        <Button title="Go back" buttonType="primary" buttonSize="md" onClick={handleClick} />
+        <Button
+          title="Go back"
+          buttonType="primary"
+          buttonSize="md"
+          onClick={() => navigate('/')}
+        />
       </div>
       <Footer />
     </div>

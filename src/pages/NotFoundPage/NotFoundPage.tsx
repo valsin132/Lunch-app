99af4ea8c493cf46f3d93fdebe062cc4a5305747
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-05-27 12:45:27 +0300

    changes

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -12,8 +12,8 @@ export function NotFoundPage() {
   return (
     <div className={cx('not-found-page__wrapper')}>
       <div className={cx('not-found-page__content')}>
-        <h2 className={cx('not-found-page__header')}>Oops!</h2>
-        <p>Page not found!</p>
+        <p className={cx('not-found-page__header')}>Oops!</p>
+        <h1>Page not found!</h1>
         <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
         <Button
           title="Go to Home page"
